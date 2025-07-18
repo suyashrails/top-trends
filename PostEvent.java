bcf0b3d0b2298e3cda978f3dee15185dc473f6dc
suyashrails <suyash.ps@gmail.com>
2025-07-18 07:23:37 -0700

    Create PostEvent.java

---
+++ PostEvent.java
@@ -0,0 +1,7 @@
+public class PostEvent {
+    private String token;
+    private String platform;
+    private int postCount, likes, comments;
+    private long timestamp;
+    // getters/setters/constructors...
+}
